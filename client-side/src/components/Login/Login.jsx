9982e7f2053cc4bd5c4fbddcfbe0f71a4074c172
Katrin Yordanova <katrinyordanova@abv.bg>
2019-12-04 18:08:38 +0200

    Adding login

--- client-side/src/components/Login/Login.jsx
+++ client-side/src/components/Login/Login.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.css';
+import userService from '../../services/userService';
 
-function Login() {
-    return <div className="Login">Login</div>
+class Login extends  Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        userService.login(this.props.username, this.props.password);
+        this.props.history.push('/');
+    }
+
+    render() {
+        return <div className="Login">
+            <form className="LoginForm">
+                <div className="Header">
+                    <h1>Login</h1>
+                </div>
+                <div className="InputFields">
+                    <div className="Username">
+                        <label>Username</label>
+                        <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                    </div>
+                    <div className="Password">
+                        <label>Password</label>
+                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="FormButton">
+                    <button type="button" onClick={this.handleSubmit}>Submit</button>
+                </div>
+            </form>
+        </div>
+    }
 }
 
 export default Login;

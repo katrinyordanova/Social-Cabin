ed819843dd67b40a0773e2e274b3ea2e6b9beeb1
Katrin Yordanova <katrinyordanova@abv.bg>
2019-12-12 14:47:29 +0200

    Fixed all styles

--- client-side/src/components/Homepage/GuestHomepage/GuestHomepage.jsx
+++ client-side/src/components/Homepage/GuestHomepage/GuestHomepage.jsx
@@ -3,16 +3,18 @@ import './GuestHomepage.css';
 import Link from '../../links/Link';
 
 export default function GuestHomepage() {
-    return <div className="Homepage">
-        <img src="/happy-penguin.gif" alt="happy-penguin"/>
-        <div className="Title">
+    return <div className="GuestHomepage">
+        <div className="GuestHomepageGif">
+            <img src="/happy-penguin.gif" alt="happy-penguin"/>
+        </div>
+        <div className="GuestHomepageTitle">
             <h1>Hello there!</h1>
             <h1>Welcome to Social Penguin!</h1>
             <br/>
             <h1>Excited?</h1>
             <h1>Let's get started!</h1>
         </div>
-        <div className="HomepageButtons">
+        <div className="GuestHomepageButtons">
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
         </div>
--- client-side/src/components/site/Contacts/Contacts.jsx
+++ client-side/src/components/site/Contacts/Contacts.jsx
@@ -3,13 +3,13 @@ import './Contacts.css';
 
 function Contacts() {
     return <div className="Contacts">
-        <h1>Have a question? Do not hesitate to ask!</h1>
-        <h2>Here are our coordinates:</h2>
-        <h3>Email: mariahristova@abv.bg</h3>
-        <h3>Address: gr. Sofia, j.k. Drujba, str. Koche Vihrogonche N3</h3>
-        <h3>Phone: 0896934762, 0885617432</h3>
-        <h4>There are no stupid questions!</h4>
-        <h5>This isn't a challenge, though.</h5>
+                <h1>Have a question? Do not hesitate to ask!</h1>
+                <h2>Here are our coordinates:</h2>
+                <h3>Email: mariahristova@abv.bg</h3>
+                <h3>Address: gr. Sofia, j.k. Drujba, str. Konche Vihrogonche №3</h3>
+                <h3>Phone: 0896934762, 0885617432</h3>
+                <h4>There are no stupid questions!</h4>
+                <h5>This isn't a challenge, though.</h5>
     </div>
 }
 

d802555b3619422e8aec4fcc30a6434b9f48fd07
Katrin Yordanova <katrinyordanova@abv.bg>
2020-06-04 21:52:12 +0300

    restyled navigation

--- client-side/src/components/core-components/Navigation/Navigation.jsx
+++ client-side/src/components/core-components/Navigation/Navigation.jsx
@@ -1,23 +1,27 @@
 import React,{ Component }  from 'react';
-import './Navigation.css';
+import './Navigation.scss';
 import Link from '../../links/Link';
 
 class Navigation extends Component {   
     render() {
         const { isLogged } = this.props;
-    return <nav className="Navigation">
+    return <nav className="navigation">
         <ul>
-            <div className="LogoAndName">
-                <img src="/penguin.png" alt="planet-icon" id="logo" />
-                <p>Social Penguin</p>
+            <div className="navigation__logo-and-name">
+                <img className="navigation__logo-and-name__logo" src="/penguin.png" alt="penguin-logo" />
+                <p className="navigation__logo-and-name__name">Social Penguin</p>
             </div>
-            { isLogged && <Link to='/'>Home</Link> }
-            { isLogged && <Link to='/new-post'>New Post</Link> }
-            {/* { isLogged && <Link to='/view-profile'>Profile</Link> }
-            { isLogged && <Link to='/my-posts'>My Posts</Link> } */}
-            { isLogged && <Link to='/logout'>Logout</Link> }
-            { isLogged && <Link to='/contacts'>Contacts</Link> }
-            { isLogged && <Link to='/about-us'>About us</Link> }
+            <div className="navigation__links">
+                { !isLogged && <Link to='/login'>Login</Link> }
+                { !isLogged && <Link to='/register'>Register</Link> }
+                { isLogged && <Link to='/'>Home</Link> }
+                { isLogged && <Link to='/new-post'>New Post</Link> }
+                {/* { isLogged && <Link to='/view-profile'>Profile</Link> }
+                { isLogged && <Link to='/my-posts'>My Posts</Link> } */}
+                { isLogged && <Link to='/logout'>Logout</Link> }
+                { isLogged && <Link to='/contacts'>Contacts</Link> }
+                { isLogged && <Link to='/about-us'>About us</Link> }
+            </div>
         </ul>
     </nav>
     }

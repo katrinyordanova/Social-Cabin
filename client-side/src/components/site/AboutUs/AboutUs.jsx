0ff06bb15c8352529f6ad72df881496bf8b419fc
Katrin Yordanova <katrinyordanova@abv.bg>
2020-06-12 18:16:03 +0300

    restyled about us component

--- client-side/src/components/site/AboutUs/AboutUs.jsx
+++ client-side/src/components/site/AboutUs/AboutUs.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import './AboutUs.css';
+import './AboutUs.scss';
 
 function AboutUs() {
-    return <div className="AboutUs">
-                <div className="AboutUsDescription">
-                    <h1>Hey, hey!</h1>
-                    <h2>Social Penguin is a social network for everyone!</h2>
-                    <h2>To get started you have to register.</h2>
-                    <h2>But if you already have an account, you know what to do! ;)</h2>
-                    <h2>Have fun and don't forget to enjoy yourself! :)</h2>
-                    <br/>
-                    <h3>We value you privacy and use cookies!</h3>
-                </div>
-            </div>
+    return <div className="about-us">
+        <div className="about-us__header">
+            <img className="about-us__tree" src="../../../tree.png" alt="tree"/>
+            <h1 className="about-us__title">About Us</h1>
+            <img className="about-us__tree" src="../../../tree.png" alt="tree"/>
+        </div>
+        <p className="about-us__description">Social Cabin is a place where you can escape from your busy routine. Join our secluted community and enjoy a moment of peace from the office or the world. Here you can read other users' posts, create your own, edit or delete them.</p>
+    </div>
 }
 
 export default AboutUs;

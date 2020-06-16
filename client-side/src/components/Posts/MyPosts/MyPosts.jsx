5826c0329214a633e1ff440f3f7dd592392ba766
Katrin Yordanova <katrinyordanova@abv.bg>
2020-06-16 15:33:50 +0300

    added logic

--- client-side/src/components/Posts/MyPosts/MyPosts.jsx
+++ client-side/src/components/Posts/MyPosts/MyPosts.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import './MyPosts.scss';
+import Post from '../Post/Post';
+import postService from '../../../services/postService';
 
-class MyPosts extends Component {
-    componentDidMount() {
-        
+export default class MyPosts extends Component {
+    state = {
+        posts: null
     }
+
+    componentDidMount() {
+        const id = localStorage.getItem('user');
+        postService.get.myPosts(id).then(posts => {
+            if(posts.length > 0) {
+                this.setState({ posts });
+            }else {
+                posts = null;
+            }
+        });
+    }
+    
     render() {
-        return <div className="MyPosts">
+        const { posts } = this.state;
+
+        return <div className="my-posts">
             <h1>My Posts</h1>
+            { posts ? 
+                <div>
+                    {posts.map((post) => 
+                        <Post key={post._id}>
+                            <div className="my-posts__title">{post.title}</div>
+                            <div className="my-posts__description">{post.description}</div>
+                        </Post>)
+                    }
+                </div> : <div>No posts yet</div>
+            }
         </div>
     }
 }
-
-export default MyPosts;

b7652494499056d0a79575dc55e708c75973c310
Katrin Yordanova <katrinyordanova@abv.bg>
2020-06-25 16:13:21 +0300

    fixed EditPost component

--- client-side/src/components/Posts/EditPost/EditPost.jsx
+++ client-side/src/components/Posts/EditPost/EditPost.jsx
@@ -1,106 +1,70 @@
 import React, { Component } from 'react';
-import './EditPost.css';
+import './EditPost.scss';
+import Post from '../Post/Post';
 import postService from '../../../services/postService';
+import editPostValidator from '../../../utils/postValidations/editPost';
 
 export default class EditPost extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            post : null
-        }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    state = {
+        post: null
     }
 
     componentDidMount() {
-        
+        const postId = this.props.match.params.id;
+        postService.get.one(postId)
+        .then(post => {
+            post = JSON.parse(post);
+            this.setState({ post });
+        });
     }
-    
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+    }
+
     handleSubmit(event) {
         event.preventDefault();
-        const data = {
-            title: this.props.title, 
-            description: this.props.description
-        }
-
-        const id = this.props.match.params.id;
-
-        postService.get.one(id, data);alert('me')
-            .then((post) => {
-                this.setState({ post });
+        const title = this.state.title;
+        const description = this.state[Object.keys(this.state)[Object.keys(this.state).length - 1]];
+        if(editPostValidator(title, description)) {
+            const data = { title, description };
+            postService.edit(this.state.post._id, data).then(() => {
+                this.props.history.push('/');
             });
+        }
     }
 
     render() {
-        return <div className="EditPost">
-           <div className="Heading">
-                <h1>Edit post</h1>
-            </div>
-            <form>
+        const { post } = this.state;
+
+        return <div className="edit-post">
+            <h1 className="edit-post__heading">Edit post</h1>
+            <form className="edit-post__form">
+            {post ?
                 <div>
-                    <label>Title</label>
-                    <input type="text">{this.props.title}</input>
-                </div>
-                <div>
-                    <label>Description</label>
-                    <textarea name="description" cols="40" rows="10">{this.props.description}</textarea>
-                </div>
-                <button type="button" onClick={this.handleSubmit}>Submit</button>
+                    <Post key={post._id}>
+                        <div className="edit-post__form__title">
+                            <label className="edit-post__form__title__label">Title</label>
+                            <input className="edit-post__form__title__input" type="text" name="title" defaultValue={post.title} onChange={this.handleChange} />
+                        </div>
+                        <div className="edit-post__form__description">
+                            <label className="edit-post__form__description__label">Description</label>
+                            <textarea className="edit-post__form__description__textarea" defaultValue={post.description} onChange={this.handleChange} cols="40" rows="10" />
+                        </div>
+                        <button className="edit-post__form__submit-button" type="button" onClick={this.handleSubmit}>Submit</button>
+                    </Post>
+                </div> : null
+            }
             </form>
         </div>
     }
 }
-// import React, { Component } from 'react';
-// import './EditPost.css';
-// import '../../shared-styles/UserForm/UserForm.css';
-// import '../../shared-styles/ProfileAndPostForms/ProfileAndPostForms.css'
-// import postService from '../../../services/postService';
-
-// class EditPost extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             title: '',
-//             description: 'What\'s on your mind'
-//         }
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//     }
-
-//     handleChange(event) {
-//         this.setState({
-//             [event.target.name]: event.target.value
-//         });
-//     }
-
-//     handleSubmit(event) {
-//         event.preventDefault();
-//         const title = this.state.title;
-//         const description = this.state.description;
-//         console.log(this.props);
-//         const postId = this.props.location.key;
-//         const data = { title, description };
-//         postService.edit(postId, data).then(() => {
-//             this.props.history.push();
-//         });
-//     }
-
-//     render() {
-//         return <div className="EditPost">
-//             <div className="Heading">
-//                 <h1>Edit post</h1>
-//             </div>
-//             <form>
-//                 <div>
-//                     <label>Title</label>
-//                     <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
-//                 </div>
-//                 <div>
-//                     <label>Description</label>
-//                     <textarea name="description" id="description" cols="40" rows="10"></textarea>
-//                 </div>
-//                 <button type="button" onClick={this.handleSubmit}>Submit</button>
-//             </form>
-//         </div>
-//     }
-// }
-
-// export default EditPost;

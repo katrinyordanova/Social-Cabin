e9665411aeb4a8ba274a3c45b7bbdf87348f768a
Katrin Yordanova <katrinyordanova@abv.bg>
2020-07-07 15:26:47 +0300

    added pop-up delete post message in MyPosts

--- client-side/src/components/Posts/MyPosts/MyPosts.jsx
+++ client-side/src/components/Posts/MyPosts/MyPosts.jsx
@@ -10,8 +10,10 @@ export default class MyPosts extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: null
+            posts: null,
+            deleteMessage: false
         }
+        this.deletePostMessage = this.deletePostMessage.bind(this);
     }
 
     componentDidMount() {
@@ -25,9 +27,21 @@ export default class MyPosts extends Component {
         });
     }
 
+    deletePostMessage(postId) {
+        this.setState({ deleteMessage: false });
+        postService.delete(postId)
+            .then(() => {
+                const posts = this.state.posts.filter(post => post._id !== postId);
+                this.setState({ posts });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     render() {
         const { posts } = this.state;
-
+        console.log(this.state.deleteMessage);
         return <div className="my-posts">
             <h1 className="my-posts__heading">My Posts</h1>
             { posts ? 
@@ -39,7 +53,12 @@ export default class MyPosts extends Component {
                                 <div className="my-posts__posts__description">{post.description}</div>
                                 <div className="my-posts__posts__buttons">
                                     <Link to={"/edit-post/" + post._id} className="my-posts__posts__buttons__edit"><FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon></Link>
-                                    <FontAwesomeIcon className="my-posts__posts__buttons__delete" icon={ faTrash }><button></button></FontAwesomeIcon>
+                                    <FontAwesomeIcon onClick={() => this.setState({ deleteMessage: true })} className="my-posts__posts__buttons__delete" icon={ faTrash }><button></button></FontAwesomeIcon>
+                                </div>
+                                <div className={this.state.deleteMessage ? "my-posts__posts__show-deleteMessage" : "my-posts__posts__hide-deleteMessage"}>
+                                    <h1>Delete this post?</h1>
+                                    <button className="my-posts__posts__show-deleteMessage__delete-button" onClick={() => this.deletePostMessage(post._id)}>Delete</button>
+                                    <button className="my-posts__posts__show-deleteMessage__cancel-button" onClick={() => this.setState( { deleteMessage: false }) }>Cancel</button>
                                 </div>
                             </Post>
                         )

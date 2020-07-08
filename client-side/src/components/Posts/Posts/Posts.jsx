f49253e984db3ed6564fcfa68cacd997cddaf789
Katrin Yordanova <katrinyordanova@abv.bg>
2020-07-08 17:03:29 +0300

    added like button to Posts

--- client-side/src/components/Posts/Posts/Posts.jsx
+++ client-side/src/components/Posts/Posts/Posts.jsx
@@ -4,8 +4,12 @@ import './Posts.scss';
 import postService from '../../../services/postService';
 
 export default class Posts extends Component {
-    state = {
-        posts: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: null
+        }
+        this.likePost = this.likePost.bind(this);
     }
 
     componentDidMount() {
@@ -14,18 +18,25 @@ export default class Posts extends Component {
         });
     }
 
+    likePost(id) {
+
+    }
+
     render() {
         const { posts } = this.state;
-
         return <div>
             {posts ? 
                 <div className="posts">
-                    {posts.map((post) =>
-                    <Post key={post._id}>
-                        <div className="posts__post__title">{post.title}</div>
-                        <div className="posts__post__description">{post.description}</div>
-                        <div className="posts__post__author">By: {post.author[0].username}</div>
-                    </Post>)}
+                    {
+                        posts.map((post) =>
+                            <Post key={post._id}>
+                                <div className="posts__post__title">{post.title}</div>
+                                <div className="posts__post__description">{post.description}</div>
+                                <div className="posts__post__author">By: {post.author[0].username}</div>
+                                <button onClick={() => { this.likePost(post._id) }}>Like</button>{post.like}
+                            </Post>
+                        )
+                    }
                 </div> : <div>Loading...</div>
             }
         </div>

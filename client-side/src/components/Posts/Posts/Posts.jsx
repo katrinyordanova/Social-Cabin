17a822bab3908dc2c49ece6f4420bfbaef6283bd
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-20 18:02:42 +0300

    converted class Posts to functional component with hooks

--- client-side/src/components/Posts/Posts/Posts.jsx
+++ client-side/src/components/Posts/Posts/Posts.jsx
@@ -1,39 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from '../Post/Post';
 import './Posts.scss';
 import postService from '../../../services/postService';
 
-export default class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: null
-        }
-    }
+export default function Posts() {
+    const [ posts, setPosts ] = useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         postService.get.many().then(posts => {
-            this.setState({ posts });
+            setPosts(posts);
         });
-    }
+    });
 
-    render() {
-        const { posts } = this.state;
-
-        return <div>
-            {posts ? 
-                <div className="posts">
-                    {
-                        posts.map((post) =>
-                            <Post key={post._id}>
-                                <div className="posts__post__title">{post.title}</div>
-                                <div className="posts__post__description">{post.description}</div>
-                                <div className="posts__post__author">By: {post.author[0].username}</div>
-                            </Post>
-                        )
-                    }
-                </div> : <div>Loading...</div>
-            }
-        </div>
-    }
+    return <div>
+        { posts ? 
+            <div className="posts">
+                {
+                    posts.map((post) =>
+                        <Post key={post._id}>
+                            <div className="posts__post__title">{post.title}</div>
+                            <div className="posts__post__description">{post.description}</div>
+                            <div className="posts__post__author">By: {post.author[0].username}</div>
+                        </Post>
+                    )
+                }
+            </div> : <div>Loading...</div>
+        }
+    </div>
 }

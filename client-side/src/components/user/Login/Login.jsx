9fab972ed54daeb000659735cb312a542dcef5fd
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-21 18:54:25 +0300

    changed class to functional component with hook

--- client-side/src/components/user/Login/Login.jsx
+++ client-side/src/components/user/Login/Login.jsx
@@ -1,61 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../shared-styles/UserForm/UserForm.scss';
 import userService from '../../../services/userService';
 import loginValidator from '../../../utils/userValidations/loginValidator/loginValidator';
 import { toast } from 'react-toastify';
+import { useFormFields } from '../../../custom-hooks/login-hook';
+// import { useHistory } from 'react-router-dom';
 
-class Login extends  Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const username = this.state.username;
-        const password = this.state.password;
-
-        if (loginValidator(username, password)) {
-            this.setState({ isLogged : true });
-            const data = { username, password };
-            userService.login(data).then(() => {
-                localStorage.setItem('user', username);
-                this.props.login(data, this.props.history)
-            })
-            .catch(error => {
+export default function Login() { 
+    const [ fields, handleFieldChange ] = useFormFields({ username: '', password: '' });
+    // const history = useHistory();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(loginValidator(fields.username, fields.password)) {
+            userService.login(fields).then(() => {
+                localStorage.setItem('user', fields.username);
+                // history.push('/');
+            }).catch(error => {
                 toast.error('Invalid username or password!');
                 return;
             });
         }
     }
 
-    render() {
-        return <div className="container">
-            <h1 className="container__header">Login</h1>
-            <form className="container__form">
-                <div className="container__form__username">
-                    <label>Username</label>
-                    <input type="text" name="username" placeholder="Enter username" value={this.state.username} onChange={this.handleChange} />
-                </div>
-                <div className="container__form__password">
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Enter password" value={this.state.password} onChange={this.handleChange} />
-                </div>
-                <button className="container__form__submit-button" onClick={this.handleSubmit}>Submit</button>
-            </form>
-        </div>
-    }
+    return <div className="container">
+             <h1 className="container__header">Login</h1>
+             <form className="container__form">
+                 <div className="container__form__username">
+                     <label>Username</label>
+                     <input type="text" name="username" placeholder="Enter username" defaultValue={fields.username} onChange={handleFieldChange} />
+                 </div>
+                 <div className="container__form__password">
+                     <label>Password</label>
+                     <input type="password" name="password" placeholder="Enter password" defaultValue={fields.password} onChange={handleFieldChange} />
+                 </div>
+                 <button className="container__form__submit-button" onClick={handleSubmit}>Submit</button>
+             </form>
+         </div>
 }
-
-export default Login;

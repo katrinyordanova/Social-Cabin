2975a61483a3feefd142d91240162853d1f55371
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-24 16:39:01 +0300

    transformed MyPosts class component to functional with hook

--- client-side/src/components/Posts/MyPosts/MyPosts.jsx
+++ client-side/src/components/Posts/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './MyPosts.scss';
 import Post from '../Post/Post';
 import Link from '../../links/Link';
@@ -6,65 +6,65 @@ import postService from '../../../services/postService';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default class MyPosts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: null,
-            deleteMessage: false
-        }
-        this.deletePostMessage = this.deletePostMessage.bind(this);
-    }
+const MyPosts = () => {
+    const [ myPosts, setMyPosts ] = useState(null);
+    const [ deleteMessage, setDeleteMessage ] = useState(false);
 
-    componentDidMount() {
+    const toggleDeleteMessage = () => setDeleteMessage(!deleteMessage);
+
+    useEffect(() => {
         const id = localStorage.getItem('user');
-        postService.get.myPosts(id).then(posts => {
-            if(posts.length > 0) {
-                this.setState({ posts });
+        postService.get.myPosts(id).then(myPosts => {
+            if(myPosts.length > 0) {
+                setMyPosts(myPosts);
             }else {
-                posts = null;
+                myPosts = null;
             }
+        }, []);
+    });
+    
+
+    const deletePostMessage = (myPostId) => {
+        postService.delete(myPostId).then(() => {
+            const posts = myPosts.filter(post => post._id !== myPostId);
+            setMyPosts(posts);
         });
     }
 
-    deletePostMessage(postId) {
-        this.setState({ deleteMessage: false });
-        postService.delete(postId)
-            .then(() => {
-                const posts = this.state.posts.filter(post => post._id !== postId);
-                this.setState({ posts });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
 
-    render() {
-        const { posts } = this.state;
-        console.log(this.state.deleteMessage);
-        return <div className="my-posts">
-            <h1 className="my-posts__heading">My Posts</h1>
-            { posts ? 
-                <div className="my-posts__posts">
-                    {
-                        posts.map((post) => 
-                            <Post key={post._id}>
-                                <div className="my-posts__posts__title">{post.title}</div>
-                                <div className="my-posts__posts__description">{post.description}</div>
-                                <div className="my-posts__posts__buttons">
-                                    <Link to={"/edit-post/" + post._id} className="my-posts__posts__buttons__edit"><FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon></Link>
-                                    <FontAwesomeIcon onClick={() => this.setState({ deleteMessage: true })} className="my-posts__posts__buttons__delete" icon={ faTrash }><button></button></FontAwesomeIcon>
-                                </div>
-                                <div className={this.state.deleteMessage ? "my-posts__posts__show-deleteMessage" : "my-posts__posts__hide-deleteMessage"}>
-                                    <h1>Delete this post?</h1>
-                                    <button className="my-posts__posts__show-deleteMessage__delete-button" onClick={() => this.deletePostMessage(post._id)}>Delete</button>
-                                    <button className="my-posts__posts__show-deleteMessage__cancel-button" onClick={() => this.setState( { deleteMessage: false }) }>Cancel</button>
-                                </div>
-                            </Post>
-                        )
-                    }
-                </div> : <div><p className="my-posts__no-posts">No posts yet</p></div>
-            }
-        </div>
-    }
+    return <div className="my-posts">
+        <h1 className="my-posts__heading">My Posts</h1>
+        { myPosts ? 
+            <div className="my-posts__posts">
+                {
+                    myPosts.map((myPost) => 
+                        <Post key={myPost._id}>
+                            <div className="my-posts__posts__title">{myPost.title}</div>
+                            <div className="my-posts__posts__description">{myPost.description}</div>
+                            <div className="my-posts__posts__buttons">
+                                <Link to={"/edit-post/" + myPost._id} className="my-posts__posts__buttons__edit">
+                                    <FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon>
+                                </Link>
+                                <FontAwesomeIcon 
+                                    onClick={toggleDeleteMessage} className="my-posts__posts__buttons__delete"
+                                    icon={ faTrash }>
+                                </FontAwesomeIcon>
+                            </div>
+                            <div className={deleteMessage ? "my-posts__posts__show-deleteMessage" : "my-posts__posts__hide-deleteMessage"}>
+                                <h1>Delete this post?</h1>
+                                <button className="my-posts__posts__show-deleteMessage__delete-button" 
+                                    onClick={() => deletePostMessage(myPost._id)}>Delete
+                                </button>
+                                <button className="my-posts__posts__show-deleteMessage__cancel-button" 
+                                    onClick={() => setDeleteMessage({ deleteMessage: false }) }>Cancel
+                                </button>
+                            </div>
+                        </Post>
+                    )
+                }
+            </div> : <div><p className="my-posts__no-posts">No posts yet</p></div>
+        }
+    </div>
 }
+
+export default MyPosts;

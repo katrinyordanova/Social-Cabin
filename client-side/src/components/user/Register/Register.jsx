baf85dfa5af7170b82381554b21740d6c16a599a
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-24 13:31:57 +0300

    converted register class into functional component with a hook

--- client-side/src/components/user/Register/Register.jsx
+++ client-side/src/components/user/Register/Register.jsx
@@ -1,39 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../shared-styles/UserForm/UserForm.scss';
 import userService from '../../../services/userService';
 import registerValidator from '../../../utils/userValidations/registerValidator/registerValidator';
 import { toast } from 'react-toastify';
+import { useFormFields } from '../../../custom-hooks/login-hook';
+import { useHistory } from 'react-router-dom';
 
-export default class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            confirmPassword: ''
-        }
+const Register = () => {
+    const [ fields, handleFieldChange ] = useFormFields({ username: '', password: '', confirmPassword: '' });
+    const history = useHistory();
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-            
-    handleChange(event) {
-        this.setState({
-            [event.target.name] : event.target.value
-        });
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const username = this.state.username; 
-        const password = this.state.password;
-        const confirmPassword = this.state.confirmPassword; 
         
-        if(registerValidator(username, password, confirmPassword)) {
-            const data = { username, password };
-            userService.register(data).then(() => {
-                localStorage.setItem('user', username);
-                this.props.history.push('/login');      
+        if(registerValidator(fields.username, fields.password, fields.confirmPassword)) {
+            const username = fields.username;
+            const password = fields.password;
+            userService.register({ username, password }).then(() => {
+                history.push('/login');      
             }).catch(() => {
                 toast.error('The username is already taken');
                 return;
@@ -41,24 +25,24 @@ export default class Register extends Component {
         }
     }
 
-    render() {
-        return <div className="container">
-            <h1 className="container__header">Register</h1>
-            <form className="container__form">
-                <div className="container__form__username">
-                    <label>Username</label>
-                    <input type="text" name="username" placeholder="Enter username" value={this.state.username} onChange={this.handleChange} />
-                </div>
-                <div className="container__form__password">
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Enter password" value={this.state.password} onChange={this.handleChange} />
-                </div>
-                <div className="container__form__confirm-password">
-                    <label>Confirm Password</label>
-                    <input type="password" name="confirmPassword" placeholder="Enter confirm password" value={this.state.confirmPassword} onChange={this.handleChange} />
-                </div>
-                <button className="container__form__submit-button" onClick={this.handleSubmit}>Submit</button>
-            </form>
-        </div>
-    }
+    return <div className="container">
+        <h1 className="container__header">Register</h1>
+        <form className="container__form">
+            <div className="container__form__username">
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Enter username" defaultValue={fields.username} onChange={handleFieldChange} />
+            </div>
+            <div className="container__form__password">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter password" defaultValue={fields.password} onChange={handleFieldChange} />
+            </div>
+            <div className="container__form__confirm-password">
+                <label>Confirm Password</label>
+                <input type="password" name="confirmPassword" placeholder="Enter confirm password" defaultValue={fields.confirmPassword} onChange={handleFieldChange} />
+            </div>
+            <button className="container__form__submit-button" onClick={handleSubmit}>Submit</button>
+        </form>
+    </div>
 }
+
+export default Register;

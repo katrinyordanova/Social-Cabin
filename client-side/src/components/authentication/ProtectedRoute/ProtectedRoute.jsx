b3bcc8c4837eb4670ef263e8503b3f38795eaa58
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-24 17:17:03 +0300

    removed unused component

--- client-side/src/components/authentication/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
-
-// const ProtectedRoute = ({ isLogged, redirectTo, ...props}) => {
-//     return isLogged ? 
-// }

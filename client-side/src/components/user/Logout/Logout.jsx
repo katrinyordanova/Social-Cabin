f63947fd6f11ecdc9f97247a0cd73cd526013ee3
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-24 13:12:45 +0300

    removed unused code

--- client-side/src/components/user/Logout/Logout.jsx
+++ client-side/src/components/user/Logout/Logout.jsx
@@ -1,16 +1,4 @@
 export default function Logout({ logout, history }) {
   logout(history);
-  localStorage.removeItem('user');
   return null;
 }
-// export default class Logout extends Component {
-//   render() {
-//     userService.logout().then(() => {
-//       this.setState({ isLogged: false });
-//       this.props.history.push('/');
-//     });
-//     return (
-//     <Redirect to="/" />
-//     ) 
-//   }
-// }

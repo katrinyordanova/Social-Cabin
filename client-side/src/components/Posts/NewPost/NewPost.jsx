27066bb2466053cffd2602d08d6bc7a18354a38d
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-24 14:17:39 +0300

    convered class component NewPost to functional

--- client-side/src/components/Posts/NewPost/NewPost.jsx
+++ client-side/src/components/Posts/NewPost/NewPost.jsx
@@ -1,54 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './NewPost.scss';
 import postService from '../../../services/postService';
 import newPostValidator from '../../../utils/postValidations/createPost';
+import { useFormFields } from '../../../custom-hooks/update-form-fields';
+import { useHistory } from 'react-router-dom';
 
-class NewPost extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: '',
-            description: 'What\'s on your mind?'
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const NewPost = () => {
+    const [ fields, handleFieldChange ] = useFormFields({ title: '', description: '' });
+    const history = useHistory();
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const title = this.state.title;
-        const description = this.state.description;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const title = fields.title;
+        const description = fields.description;
         const username = localStorage.getItem('user');
         if(newPostValidator(title, description)) {
-            const data = { title , description, username };
-            postService.create(data).then(() => {
-                this.props.history.push('/');
+            postService.create({ title , description, username }).then(() => {
+                history.push('/');
             });
         }
     }
 
-    render() {
-        return <div className="new-post">
-            <h1 className="new-post__heading">Create a new post</h1>
-            <form className="new-post__form">
-                <div className="new-post__form__title">
-                    <label className="new-post__form__title__label">Title</label>
-                    <input className="new-post__form__title__input" type="text" name="title" placeholder="Write a title" value={this.props.title} onChange={this.handleChange} />
-                </div>
-                <div className="new-post__form__description">
-                    <label className="new-post__form__description__label">Description</label>
-                    <textarea className="new-post__form__description__textarea" name="description" placeholder="What's on your mind?" value={this.state.value} onChange={this.handleChange} cols="40" rows="10" />
-                </div>
-                <button className="new-post__form__submit-button" type="button" onClick={this.handleSubmit}>Submit</button>
-            </form>
-        </div>
-    }
+    return <div className="new-post">
+        <h1 className="new-post__heading">Create a new post</h1>
+        <form className="new-post__form">
+            <div className="new-post__form__title">
+                <label className="new-post__form__title__label">Title</label>
+                <input className="new-post__form__title__input" type="text" name="title" placeholder="Write a title" defaultValue={fields.title} onChange={handleFieldChange} />
+            </div>
+            <div className="new-post__form__description">
+                <label className="new-post__form__description__label">Description</label>
+                <textarea className="new-post__form__description__textarea" name="description" placeholder="What's on your mind?" defaultValue={fields.value} onChange={handleFieldChange} cols="40" rows="10" />
+            </div>
+            <button className="new-post__form__submit-button" type="button" onClick={handleSubmit}>Submit</button>
+        </form>
+    </div>
 }
 
 export default NewPost;

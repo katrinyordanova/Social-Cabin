88efe7007d9d4ff350c3f1208c04e093ca85c243
Katrin Yordanova <katrinyordanova@abv.bg>
2020-08-25 17:38:44 +0300

    changed whole component

--- client-side/src/components/site/NotFound/NotFound.jsx
+++ client-side/src/components/site/NotFound/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './NotFound.css';
+import './NotFound.scss';
+import Link from '../../links/Link';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
-export default function NotFound() {
-    return <div className="NotFound">
-        <img src="/notFound.jpeg" alt="not-found"/>
-        <h2>Looks like you're trying to find something, but it isn't there.</h2>
-        <h2>Try something else.</h2>
+const NotFound = ({ isLogged }) => {
+return <div className="not-found">
+        <img className="not-found__picture" src="/pictures/not-found.png" alt="not-found" />
+        { !isLogged && <Link className="not-found__guest-button" to='/homepage'><FontAwesomeIcon icon={ faHome }></FontAwesomeIcon> Home</Link> }
+        { isLogged && <Link className="not-found__user-button" to='/'><FontAwesomeIcon icon={ faHome }></FontAwesomeIcon> Home</Link> }
     </div>
 }
+
+export default NotFound;

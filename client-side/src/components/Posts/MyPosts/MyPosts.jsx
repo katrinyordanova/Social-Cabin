57497f3299e2d1d62667d49a2464c85a15650d53
Katrin Yordanova <katrinyordanova@abv.bg>
2020-09-01 16:23:33 +0300

    fixed not working delete button

--- client-side/src/components/Posts/MyPosts/MyPosts.jsx
+++ client-side/src/components/Posts/MyPosts/MyPosts.jsx
@@ -9,31 +9,37 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 const MyPosts = () => {
     const [ myPosts, setMyPosts ] = useState(null);
     const [ deleteMessage, setDeleteMessage ] = useState(false);
+    const [ deleteMyPostId, setDeleteMyPostId ] = useState(null);
 
     const toggleDeleteMessage = () => setDeleteMessage(!deleteMessage);
 
+    const handleDeleteMessage = (id) => {
+        setDeleteMessage(!deleteMessage);
+        setDeleteMyPostId(id);
+    }
+        
     useEffect(() => {
         const id = localStorage.getItem('user');
         postService.get.myPosts(id).then(myPosts => {
             if(myPosts.length > 0) {
                 setMyPosts(myPosts);
-                console.log(myPosts);
             }else {
                 myPosts = null;
             }
         });
     }, []);
     
-    const deletePostMessage = (myPostId) => {
-        postService.delete(myPostId).then(() => {
-            const posts = myPosts.filter(post => post._id !== myPostId);
+    const deletePostMessage = () => {
+        postService.delete(deleteMyPostId).then(() => {
+            const posts = myPosts.filter(post => post._id !== deleteMyPostId);
             setMyPosts(posts);
+            toggleDeleteMessage();
         });
     }
 
     return <div className="my-posts">
         <h1 className="my-posts__heading">My Posts</h1>
-        { myPosts ? 
+        { myPosts ?
             <div className="my-posts__posts">
                 {
                     myPosts.map((myPost) => 
@@ -45,18 +51,18 @@ const MyPosts = () => {
                                     <FontAwesomeIcon icon={ faEdit }></FontAwesomeIcon>
                                 </Link>
                                 <FontAwesomeIcon 
-                                    onClick={toggleDeleteMessage} className="my-posts__posts__buttons__delete"
+                                    onClick={() => handleDeleteMessage(myPost._id)} className="my-posts__posts__buttons__delete"
                                     icon={ faTrash }>
                                 </FontAwesomeIcon>
-                            </div>
-                            <div className={deleteMessage ? "my-posts__posts__show-deleteMessage" : "my-posts__posts__hide-deleteMessage"}>
-                                <h1>Delete this post?</h1>
-                                <button className="my-posts__posts__show-deleteMessage__delete-button" 
-                                    onClick={() => deletePostMessage(myPost._id)}>Delete
-                                </button>
-                                <button className="my-posts__posts__show-deleteMessage__cancel-button" 
-                                    onClick={toggleDeleteMessage}>Cancel
-                                </button>
+                                <div className={deleteMessage ? "my-posts__posts__show-deleteMessage" : "my-posts__posts__hide-deleteMessage"}>
+                                    <h1>Delete this post?</h1>
+                                    <button className="my-posts__posts__show-deleteMessage__delete-button" 
+                                        onClick={() => deletePostMessage()}>Delete
+                                    </button>
+                                    <button className="my-posts__posts__show-deleteMessage__cancel-button" 
+                                        onClick={toggleDeleteMessage}>Cancel
+                                    </button>
+                                </div>
                             </div>
                         </Post>
                     )

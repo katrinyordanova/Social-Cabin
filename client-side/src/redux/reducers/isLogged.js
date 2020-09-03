71e3cfa383accd4dcc7ffac3f1b698700c01e340
Katrin Yordanova <katrinyordanova@abv.bg>
2020-09-03 15:00:23 +0300

    fixed typo

--- client-side/src/redux/reducers/isLogged.js
+++ client-side/src/redux/reducers/isLogged.js
@@ -1,6 +1,6 @@
 const loggedReducer = (state = false, action) => {
     switch(action.type) {
-        case 'SIGN_IN': return !state; break;
+        case 'SIGN_IN': return !state;
         default: return state
     }
 }

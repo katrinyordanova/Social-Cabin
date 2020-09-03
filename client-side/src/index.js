39249664a19a542f45bd8531ef988d1042f4032d
Katrin Yordanova <katrinyordanova@abv.bg>
2020-09-03 15:37:14 +0300

    added redux to app via index.js

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/core-components/App/App';
+import { createStore } from 'redux';
+import loggedReducer from './redux/reducers/isLogged';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(loggedReducer, composeWithDevTools());
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
